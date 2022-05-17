348426fa55b2686d9120621b2a74fd74e13ad938
shannonasor <shannonreed@asordevelopment.com>
2022-05-17 11:10:28 -0700

    adding responsive styles for tablet mobile landing page

--- src/responsive-components/tablet-mobile/DaysTimer/DaysTimer.js
+++ src/responsive-components/tablet-mobile/DaysTimer/DaysTimer.js
@@ -9,10 +9,10 @@ class DaysTimer extends Component {
   }
   render() {
     return (
-      <div className="days-timer-big-screen">
-        <div className="days-timer-title-date-big-screen">
-          <div className="days-timer-title-big-screen">Countdown To Graduation</div>
-          <div className="days-timer-date-big-screen">{this.state.deadline}</div>
+      <div className="days-timer-tablet-mobile">
+        <div className="days-timer-title-date-tablet-mobile">
+          <div className="days-timer-title-tablet-mobile">Countdown To Graduation</div>
+          <div className="days-timer-date-tablet-mobile">{this.state.deadline}</div>
         </div>
         <DaysCounter deadline={this.state.deadline} />
       </div>
--- src/responsive-components/tablet-mobile/component.js
+++ src/responsive-components/tablet-mobile/component.js
@@ -13,8 +13,19 @@ function TabletMobile() {
     return (
         <div className="Landing-tablet-mobile" style={{ backgroundImage: "url(/keya5.jpg)", backgroundPosition: 'center',
                                                       backgroundSize: 'cover', backgroundRepeat: 'no-repeat',}} >
+
+
+
+
                                         
               <header className="Landing-header-tablet-mobile">
+
+              <div className="main-content-tablet-mobile">
+                <DaysTimer />
+              </div>
+
+
+                <div className="Landing-header-all-div-tablet-mobile">
                 <div className="Landing-header-div-tablet-mobile">
                    <h1 className="HeaderText-tablet-mobile Main-name-tablet-mobile">Shakeya Bell</h1>
                    <img src={logo} className="Landing-logo-tablet-mobile" alt="logo" />
@@ -22,12 +33,18 @@ function TabletMobile() {
                 </div>
           {/*2022 Icon attribute*/}
           {/* <a href="https://www.flaticon.com/free-icons/2022" title="2022 icons">2022 icons created by Vector Squad - Flaticon</a> */}
-              </header>
 
-              <div className="main-content">
-                <DaysTimer />
+          <div className='buttons-tablet-mobile'>
+              <button className='button-tablet-mobile button1-tablet-mobile'>Send Well Wishes</button>
+                <button className='button-tablet-mobile button2-tablet-mobile'>Virtual Graduation Party</button>
+              </div>
               </div>
 
+              
+              </header>
+
+
+             
       </div>
     )
 }
