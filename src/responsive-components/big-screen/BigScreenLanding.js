ccaafc8ad01ea1528396d5d533af823bd7be7cd7
shannonasor <shannonreed@asordevelopment.com>
2022-05-18 00:46:32 -0700

    fixing broken code

--- src/responsive-components/big-screen/BigScreenLanding.js
+++ src/responsive-components/big-screen/BigScreenLanding.js
@@ -9,32 +9,35 @@ import './styles.css'
 
 
 
-function ExSmallMobileLanding() {
+function BigScreenLanding() {
     return (
-        <div className="Landing-ex-small-mobile" style={{ backgroundImage: "url(/keya5.jpg)", backgroundPosition: 'left 35% bottom',
+        <div className="Landing-big-screen" style={{ backgroundImage: "url(/keya5.jpg)", backgroundPosition: 'left 35% bottom',
                                                       backgroundSize: 'cover', backgroundRepeat: 'no-repeat',}} >
                                         
-              <header className="Landing-header-ex-small-mobile">
-                <div className="Landing-header-div-ex-small-mobile">
-                   <h1 className="HeaderText-ex-small-mobile Main-name-ex-small-mobile">Shakeya Bell</h1>
-                   <img src={logo} className="Landing-logo-ex-small-mobile" alt="logo" />
-                   <h1 className="HeaderText-ex-small-mobile Main-graduate-ex-small-mobile">Graduate</h1>
-                </div>
-              </header>
+          <header className="Landing-header-big-screen">
 
-              <div className='buttons-ex-small-mobile'>
-                <Link to='/wellwishes'>
-                  <button className='button-ex-small-mobile button1-ex-small-mobile'>Send Well Wishes</button>
+            <div className="main-content-big-screen">
+              <DaysTimer />
+            </div>
+
+            <div className="Landing-header-all-div-big-screen">
+              <div className="Landing-header-div-big-screen">
+                <h1 className="HeaderText-big-screen Main-name-big-screen">Shakeya Bell</h1>
+                <img src={logo} className="Landing-logo-big-screen" alt="logo" />
+                <h1 className="HeaderText-big-screen Main-graduate-big-screen">Graduate</h1>
+              </div>
+
+              <div className='buttons-big-screen'>
+                <Link to="/wellwishes">
+                  <button className='button-big-screen button1-big-screen'>Send Well Wishes</button>
                 </Link>
-
+                
                 <Link to='/partyaccess'>
-                <button className='button-ex-small-mobile button2-ex-small-mobile'>Virtual Graduation Party</button>
+                  <button className='button-big-screen button2-big-screen'>Virtual Graduation Party</button>
                 </Link>
               </div>
-
-              <div className="main-content-ex-small-mobile">
-                <DaysTimer />
-              </div>
+            </div>
+            </header>
                          {/*2022 Icon attribute*/}
                          {/* <a href="https://www.flaticon.com/free-icons/2022" title="2022 icons">2022 icons created by Vector Squad - Flaticon</a> */}
               
@@ -43,4 +46,4 @@ function ExSmallMobileLanding() {
     )
 }
 
-export default ExSmallMobileLanding;
+export default BigScreenLanding;
