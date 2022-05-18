e93208610bc055492a01deec55241f3a00a4b97e
shannonasor <shannonreed@asordevelopment.com>
2022-05-17 19:51:56 -0700

    added routes with react router

---
+++ src/responsive-components/ex-small-mobile/WellWishes.js
@@ -0,0 +1,16 @@
+import React from "react";
+// importing Link from react-router-dom to navigate to 
+// different end points.
+// import { Link } from "react-router-dom";
+  
+const WellWishes = () => {
+  return (
+    <div>
+      <h1>WellWishes</h1>
+      <br />
+      
+    </div>
+  );
+};
+  
+export default WellWishes;
--- src/responsive-components/ex-small-mobile/component.js
+++ src/responsive-components/ex-small-mobile/component.js
@@ -1,39 +1,54 @@
-import React from 'react'
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 import DaysTimer from './DaysTimer/DaysTimer';
 import logo from '../../img/2022icon.png';
 import './styles.css'
 
 
-
+import ExSmallMobileLanding from './ExSmallMobileLanding';
+import WellWishes from './WellWishes';
 
 
 
 function ExSmallMobile() {
     return (
-        <div className="Landing-ex-small-mobile" style={{ backgroundImage: "url(/keya5.jpg)", backgroundPosition: 'left 35% bottom',
-                                                      backgroundSize: 'cover', backgroundRepeat: 'no-repeat',}} >
-                                        
-              <header className="Landing-header-ex-small-mobile">
-                <div className="Landing-header-div-ex-small-mobile">
-                   <h1 className="HeaderText-ex-small-mobile Main-name-ex-small-mobile">Shakeya Bell</h1>
-                   <img src={logo} className="Landing-logo-ex-small-mobile" alt="logo" />
-                   <h1 className="HeaderText-ex-small-mobile Main-graduate-ex-small-mobile">Graduate</h1>
-                </div>
-              </header>
-
-              <div className='buttons-ex-small-mobile'>
-                <button className='button-ex-small-mobile button1-ex-small-mobile'>Send Well Wishes</button>
-                <button className='button-ex-small-mobile button2-ex-small-mobile'>Virtual Graduation Party</button>
-              </div>
-
-              <div className="main-content-ex-small-mobile">
-                <DaysTimer />
-              </div>
+        <div>
+                  {/* This is the alias of BrowserRouter i.e. Router */}
+      <Router>
+        <Routes>
+          {/* This route is for home component 
+          with exact path "/", in component props 
+          we passes the imported component*/}
+          <Route path="/" element={<ExSmallMobileLanding/>} exact  />
+            
+          {/* This route is for about component 
+          with exact path "/about", in component 
+          props we passes the imported component*/}
+          <Route path="/wellwishes" element={<WellWishes/>} exact   />
+            
+          {/* This route is for contactus component
+          with exact path "/contactus", in 
+          component props we passes the imported component*/}
+          {/* <Route path="/contactus" component={ContactUs} /> */}
+            
+          {/* If any route mismatches the upper 
+          route endpoints then, redirect triggers 
+          and redirects app to home component with to="/" */}
+          {/* <Navigate to="/" /> */}
+        </Routes>
+      </Router>
                          {/*2022 Icon attribute*/}
                          {/* <a href="https://www.flaticon.com/free-icons/2022" title="2022 icons">2022 icons created by Vector Squad - Flaticon</a> */}
               
 
-      </div>
+        </div>
     )
 }
 
